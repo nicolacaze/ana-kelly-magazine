cde51b5539d99c00c5cc38fcfbbeccd8f1db421a
Nicolas <nicolas.lacaze@gmail.com>
2020-02-28 19:36:53 +0100

    Add social media links to Layout

--- src/components/layout.js
+++ src/components/layout.js
@@ -38,6 +38,20 @@ const Footer = styled.footer`
   text-align: center;
 `
 
+const SocialContainer = styled.div`
+  margin: 1rem 0 2rem;
+  svg {
+    margin-right: 1rem;
+    color: #fff;
+  }
+`
+
+const SocialIcon = ({ link, icon, size }) => (
+  <a href={link}>
+    <FontAwesomeIcon icon={icon} size={size}/>
+  </a>
+)
+
 const Layout = ({ children }) => {
   const data = useStaticQuery(graphql`
     query SiteTitleQuery {
@@ -49,20 +63,17 @@ const Layout = ({ children }) => {
     }
   `)
 
-
   return (
     <Grid>
       <Header siteTitle={data.site.siteMetadata.title} />
       <Main>{children}</Main>
       <Footer>
-        <div>
-          <FontAwesomeIcon icon={faFacebook} size='2x'/>
-          <FontAwesomeIcon icon={faInstagram} size='2x'/>
-          <FontAwesomeIcon icon={faLinkedin} size='2x'/>
-          <FontAwesomeIcon icon={faEnvelope} size='2x'/>
-        
-
-        </div>
+        <SocialContainer>
+          <SocialIcon link='https://www.instagram.com/anakellymagazine/' icon={faInstagram} size='2x' />
+          <SocialIcon link='https://www.facebook.com/anakellymagazine/' icon={faFacebook} size='2x' />
+          <SocialIcon link='https://www.linkedin.com/in/ana-kelly-6a8718168/' icon={faLinkedin} size='2x' />
+          <SocialIcon link='mailto:anakellymagazine@gmail.com' icon={faEnvelope} size='2x' />
+        </SocialContainer>
         <p>© {new Date().getFullYear()}  - Ana Kelly Magazine - Tous droits réservés</p>
       </Footer>
     </Grid>
