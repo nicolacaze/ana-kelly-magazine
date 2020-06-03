74de4b6fa04f8c8031da6ef3ba70b7136f82fad4
Nicolas <nicolas.lacaze@gmail.com>
2020-06-03 20:41:26 +0200

    Define fixed 6-tiled grid

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,20 +5,25 @@ import styled from 'styled-components'
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 import Slider from "../components/slider"
+import logo from "../images/anakelly_logo.svg"
 
-
-
-const Mosaic = styled.div`
-  display: grid;
-  grid-template-columns: repeat(auto-fill, 100px);
-  grid-auto-rows: 100px;
-  
-`
 
 const HomeLayout = styled.div`
   display: grid;
-  grid-template-columns: 40% 60%;
+  grid-template-columns: 300px 1fr;
+  padding: 2rem;
+  max-width: 1200px;
+  margin: auto;
 `
+
+const Mosaic = styled.div`
+  display: grid;
+  grid-template-columns: repeat(auto-fill, minmax(100px, 1fr));
+  grid-auto-rows: 100px;
+  margin-left: 1rem;
+  grid-gap: 0.2rem;
+`
+
 
 const Menu = styled.nav`
   border: 2px solid #000;
@@ -29,12 +34,49 @@ const Menu = styled.nav`
     margin: 0;
   }
 `
+const getVerticalSpan = index => {
+  switch(index) {
+    case 0:
+      return 5;
+    case 1:
+      return 2;
+    case 2:
+      return 3;
+    case 3:
+      return 2;
+    case 4:
+      return 2;
+    case 5:
+      return 4;
+    case 6:
+      return 3;
+  }
+}
+
+const getHorizontalSpan = index => {
+  switch(index) {
+    case 0:
+      return 4;
+    case 1:
+      return 4;
+    case 2:
+      return 6;
+    case 3:
+      return 5;
+    case 4:
+      return 5;
+    case 5:
+      return 4;
+    case 6:
+      return 3;
+  }
+}
 
 const MosaicTile = styled.div`
   overflow: hidden;
   display: grid;
-  grid-column: span ${props => props.vertical};
-  grid-row: span ${props => props.horizontal};
+  grid-column: span ${({ itemNumber }) => getVerticalSpan(itemNumber)};
+  grid-row: span ${({ itemNumber }) => getHorizontalSpan(itemNumber)};
   img {
     grid-column: 1 / -1;
     grid-row: 1 / -1;
@@ -44,14 +86,26 @@ const MosaicTile = styled.div`
   }
 `
 
+const MenuContainer = styled.div`
+  // margin-top: 5rem;
+`
+
+const Logo = styled.img`
+  position: relative;
+  top: 250px;
+  left: -240px;
+  transform: rotate(-90deg);
+  width: 530px;
+`
+
 const generateRandomNumber = (limit) => Math.floor(Math.random() * limit + 1)
 
 export default ({ data }) => {
-  console.log(data.pictures)
   return (
     <HomeLayout>
       <SEO title="home" />
-      <div>
+      <MenuContainer>
+        <Logo src={logo} alt="Ana Kelly logo"/>
         <Menu>
           <ul className="menuItems">
             <li>Ana Kelly magazine</li>
@@ -60,10 +114,12 @@ export default ({ data }) => {
             <li>Ana Kelly</li>
           </ul>
         </Menu>
-      </div>
+      </MenuContainer>
       <Mosaic>
-        {data.pictures.edges.slice(0,5).map(({ node }) => 
-          <MosaicTile key={node.id} vertical={generateRandomNumber(4)} horizontal={generateRandomNumber(4)}>
+        {console.log(data.pictures.edges)}
+        {data.pictures.edges.slice(0,7)
+        .map(({ node }, i) => 
+          <MosaicTile key={node.id} itemNumber={i}>
             <img src={node.jetpack_featured_media_url} alt="pics"/>
           </MosaicTile>
         )}
